31a17f4382b03d3e13d7dde1a903c121b7b3f969
Romana Bezdekova <64729239+11nena@users.noreply.github.com>
2024-05-05 19:21:13 +0200

    update current timezone

--- javascript/index.js
+++ javascript/index.js
@@ -25,6 +25,9 @@ parisTimeElement.innerHTML =  parisTime.format("h:mm:ss [<small>]A[</small>]");
 
 function updateCity(event) {
     let cityTimeZone = event.target.value;
+    if(cityTimeZone === "current") {
+        cityTimeZone = moment.tz.guess();
+    }
     let cityName = cityTimeZone.replace('_', ' ').split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
@@ -40,7 +43,7 @@ function updateCity(event) {
     `;
     
     
-    console.log(cityTime.format("MMMM Do YYYY"));
+    console.log(cityName);
 }
 
 updateTime();
